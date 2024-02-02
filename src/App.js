3d9f521f1981bd09cfe1dd1444dbc4f600d0fec2
patrick <patkennard1990@gmail.com>
2024-02-02 13:08:28 -0500

    added rough modal functionality, will need to customize and replace all alert

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 // import logo from './logo.svg';
 import './App.scss';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { useState } from 'react';
 
 import Board from './pages/Board/Board';
 import Main from './pages/Main/Main';
+import Alert from './components/Alert/Alert';
+
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+      setIsOpen(true);
+  };
+
+  const closeModal = () => {
+      setIsOpen(false);
+  };
+  
   return (
     <div className="App">
+            <div>
+                <button onClick={openModal} className='modal-button'>Open Modal</button>
+                <Alert isOpen={isOpen} onClose={closeModal} message="This is the modal content." />
+            </div>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Main/>}>
--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
-function Alert({ message, onClose }) {
+import './Alert.scss';
+
+function Alert({ message, onClose, isOpen }) {
+    // Conditionally render the modal based on the isOpen prop
+    const displayStyle = isOpen ? { display: 'flex' } : { display: 'none' };
+
     return (
-        <div className="modal" style={{ display: 'block' }}>
+        <div className="modal" style={displayStyle}>
             <div className="modal-content">
-                <span className="close" onClick={onClose}>&times;</span>
+                <span className="close" onClick={onClose}>close</span>
                 <p>{message}</p>
             </div>
         </div>
