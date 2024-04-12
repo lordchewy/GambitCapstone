c04bd779610a5568c11d9b1981bc4ecc8d7d0487
patrick <patkennard1990@gmail.com>
2024-04-12 11:59:11 -0400

    todo-list

--- src/pages/Test/Test.js
+++ src/pages/Test/Test.js
@@ -1,16 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Modal from '../../components/Modal/Modal';
 import './Test.scss'
-
-
-
-
 
 
 const Test = () => {
     return (
-        <Modal/>
+        <div className='todo'>
+            hello!
+        </div>
     );
 };
 
