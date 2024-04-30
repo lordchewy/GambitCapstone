2e4f66e95d5d177dd167e34236cc95626800a1b0
patrick <patkennard1990@gmail.com>
2024-04-30 11:36:12 -0400

    small todo additions

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,33 +1,40 @@
-import react, {useState} from 'react'
-import './ToDoList.scss'
+import React, { useState, useEffect } from 'react';
+import './ToDoList.scss';
 
+function ToDoList() {
+    const [tasks, setTasks] = useState([]);
+    const [newTask, setNewTask] = useState('');
 
-function ToDoList(){
-    const [tasks,setTasks] = useState([])
-    const [newTask,setNewTask] = useState('')
+    useEffect(() => {
+        const storedTasks = localStorage.getItem('tasks');
+        if (storedTasks) {
+            setTasks(JSON.parse(storedTasks));
+        }
+    }, []); // Empty dependency array to run only on mount
 
-    function handleInputChange(event){
-        // event.preventDefault();
-        setNewTask(event.target.value)
-    }
-    
-    function addTask(){
-        if(newTask.trim() !== ""){
-            setTasks(task => [...task, newTask])
-            setNewTask('')
+    useEffect(() => {
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }, [tasks]); // Save tasks whenever tasks change
+
+    function addTask() {
+        if (newTask !== "") {
+            setTasks(prevTasks => [...prevTasks, newTask]);
+            setNewTask('');
         }
     }
 
-    function deleteTask(index){
-        const updatedTasks =tasks.filter((_,i)=> i !== index);
+    function deleteTask(index) {
+        const updatedTasks = tasks.filter((_, i) => i !== index);
         setTasks(updatedTasks);
-
     }
 
-    function moveTaskUp(index){}
+    function updateTask(index, updatedTask) {
+        const updatedTasks = [...tasks];
+        updatedTasks[index] = updatedTask;
+        setTasks(updatedTasks);
+    }
 
-    
-    return(
+    return (
         <div className='todo-list'>
             <h1>ToDoList</h1>
             <div className='todo-input'>
@@ -35,27 +42,28 @@ function ToDoList(){
                     type='text'
                     placeholder='Enter new task'
                     value={newTask}
-                    onChange={handleInputChange}
+                    onChange={(event) => setNewTask(event.target.value)}
                 />
-                <button className='todo-button' onClick={addTask}>
+                <button className='todo-input__btn' onClick={addTask}>
                     Add Task
                 </button>
             </div>
 
             <ul>
-                {tasks.map((task,index)=>
-                <li key={index} className='todo-item'>
-                    <span>{task}</span>
-                    <button className='todo-button' onClick={()=> deleteTask(index)}>
-                        Delete
-                    </button>
-                    <button className='todo-button' onClick={()=> moveTaskUp(index)}>
-                        Update
-                    </button>
-                
-                </li>)}
+                {tasks.map((task, index) =>
+                    <li key={index} className='todo-item'>
+                        <span>{task}</span>
+                        <button className='todo-button' onClick={() => deleteTask(index)}>
+                            Delete
+                        </button>
+                        <button className='todo-button' onClick={() => updateTask(index)}>
+                            Update
+                        </button>
+                    </li>
+                )}
             </ul>
         </div>
-    )
+    );
 }
+
 export default ToDoList;
